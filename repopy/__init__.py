91eb88568845b97d8f8881999d454644058382b1
Jonathan Lloyd <jonathan@thisisjonathan.com>
2020-12-27 12:31:33 +0000

    Clean up __init__

--- repopy/__init__.py
+++ repopy/__init__.py
@@ -1,10 +1,16 @@
 """Interface (and default implementation set)
 for the Repository Pattern in Python"""
 
-from .repository import BackendProtocol, RepositoryFactory, Repository, RepositoryProtocol
+from .repository import (
+    BackendProtocol,
+    RepositoryFactory,
+    RepositoryProtocol,
+)
 
 __version__ = '0.1.0'
 
 __all__ = [
-    'Repository',
+    'BackendProtocol',
+    'RepositoryFactory',
+    'RepositoryProtocol',
 ]
