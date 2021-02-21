57701bc7d246d904afdfddb7e9977cca15c507f0
Jonathan Lloyd <jonathan@thisisjonathan.com>
2021-02-21 15:42:18 +0000

    First pass of middleware implementation

--- repopy/repository.py
+++ repopy/repository.py
@@ -3,6 +3,7 @@
 from datetime import datetime
 from typing import (
     Any,
+    Callable,
     Dict,
     Generic,
     List,
@@ -19,13 +20,66 @@ Field = Union[int, float, bool, str, datetime]
 FIELD_TYPES = (int, float, bool, str, datetime)
 
 EntityType = TypeVar('EntityType')
-FilterType = TypeVar('FilterType', contravariant=True)
-UpdatesType = TypeVar('UpdatesType', contravariant=True)
+FilterType = TypeVar('FilterType')
+UpdatesType = TypeVar('UpdatesType')
+
+
+AddMethodType = Callable[
+    [List[EntityType]],
+    None,
+]
+QueryMethodType = Callable[
+    [FilterType, Optional[int]],
+    List[EntityType],
+]
+UpdateMethodType = Callable[
+    [UpdatesType, FilterType],
+    int,
+]
+DeleteMethodType = Callable[
+    [FilterType],
+    int,
+]
+
+
+class RepositoryMiddleware(Generic[EntityType, FilterType, UpdatesType]):
+    def add(
+        self,
+        entities: List[EntityType], 
+        next_func: AddMethodType[EntityType],
+    ):
+        return next_func(entities)
+
+    def query(
+        self,
+        filters: FilterType,
+        next_func: QueryMethodType[FilterType, EntityType],
+        limit: int = None,
+    ) -> List[EntityType]:
+        return next_func(filters, limit)
+
+    def update(
+        self,
+        updates: UpdatesType,
+        filters: FilterType,
+        next_func: UpdateMethodType[UpdatesType, FilterType],
+    ) -> int:
+        return next_func(updates, filters)
+
+    def delete(
+        self,
+        filters: FilterType,
+        next_func: DeleteMethodType[FilterType],
+    ) -> int:
+        return next_func(filters)
 
 
 class RepositoryProtocol(Protocol, Generic[EntityType, FilterType, UpdatesType]):
     """Top-level protocol for the repository. Application code that depends on
     the repository should use this type"""
+
+    def with_middleware(self, middleware: RepositoryMiddleware[EntityType, FilterType, UpdatesType]) -> "RepositoryProtocol[EntityType, FilterType, UpdatesType]":
+        pass
 
     def add(self, entities: List[EntityType]):
         """Insert one or more new records into the repository"""
@@ -73,33 +127,89 @@ class Repository(Generic[EntityType, FilterType, UpdatesType]):
     """Default implementation of the repopy Repository Protocol"""
 
     _backend: BackendProtocol
+    _middleware: List[RepositoryMiddleware]
     _field_names: List[str]
 
-    def __init__(self, backend, field_names):
+    def __init__(self, backend: BackendProtocol, field_names: List[str]) -> None:
         self._backend = backend
+        self._middleware = []
         self._field_names = field_names
+
+    def with_middleware(self, middleware: RepositoryMiddleware[EntityType, FilterType, UpdatesType]) -> RepositoryProtocol[EntityType, FilterType, UpdatesType]:
+        repo = Repository[EntityType, FilterType, UpdatesType](
+            self._backend,
+            self._field_names,
+        )
+        repo._middleware = self._middleware + [middleware]
+        return repo
 
     def add(self, entities: List[EntityType]):
         """Insert one or more new records into the repository"""
-        self._backend.add(entities)
+        def compose(middleware_add, next_func):
+            def add(entities):
+                return middleware_add(entities, next_func)
+            return add
+
+        add_func = self._backend.add
+        for middleware in self._middleware:
+            add_func = compose(middleware.add, add_func)
+
+        return add_func(entities)
 
     def query(self, filters: FilterType, limit: int=None) -> List[EntityType]:
         """Return the records in the repository that match the given filters
         (up to the limit, if given)"""
         field_map = self._to_field_map(filters)
-        return self._backend.query(field_map, limit)
+
+        def compose(middleware_query, next_func):
+            def query(filters, limit):
+                return middleware_query(
+                    filters=filters,
+                    limit=limit,
+                    next_func=next_func,
+                )
+            return query
+
+        query_func = self._backend.query
+        for middleware in self._middleware:
+            query_func = compose(middleware.query, query_func)
+
+        return query_func(field_map, limit)
 
     def update(self, updates: UpdatesType, filters: FilterType) -> int:
         """Update the provided fields on all records that match the given
         filters"""
-        updates_map = self._to_field_map(updates)
-        filters_map = self._to_field_map(filters)
-        return self._backend.update(updates_map, filters_map)
+
+        def compose(middleware_update, next_func):
+            def update(updates, filters) -> int:
+                return middleware_update(updates, filters, next_func)
+            return update
+
+        def update_func(updates: UpdatesType, filters: FilterType) -> int:
+            updates_map = self._to_field_map(updates)
+            filters_map = self._to_field_map(filters)
+            return self._backend.update(updates_map, filters_map)
+
+        for middleware in self._middleware:
+            update_func = compose(middleware.update, update_func)
+
+        return update_func(updates, filters)
 
     def delete(self, filters: FilterType) -> int:
         """Remove all records from the repository that match the given filter"""
-        filters_map = self._to_field_map(filters)
-        return self._backend.delete(filters_map)
+
+        def compose(middleware_delete, next_func):
+            def delete(filters):
+                return middleware_delete(filters, next_func)
+
+        def delete_func(filters: FilterType) -> int:
+            filters_map = self._to_field_map(filters)
+            return self._backend.delete(filters_map)
+
+        for middleware in self._middleware:
+            delete_func = compose(middleware.delete, delete_func)
+
+        return delete_func(filters)
 
     def _to_field_map(self, obj: Any) -> Mapping[str, Field]:
         field_map = {}
@@ -118,7 +228,7 @@ class RepositoryFactory(Generic[EntityType, FilterType, UpdatesType]): # pylint:
         filter_cls: Type[FilterType],
         updates_cls: Type[UpdatesType],
         backend: BackendProtocol,
-    ) -> Repository[EntityType, FilterType, UpdatesType]:
+    ) -> RepositoryProtocol[EntityType, FilterType, UpdatesType]:
         """Create a new repository with the provided types and backend"""
         fields: Dict[str, Field] = {}
         for field_name, field_type in entity_cls.__annotations__.items():
